006eb272c1675722f19945e14ffc4cd5910987ee
tsenturion <80580892+tsenturion@users.noreply.github.com>
2025-10-22 14:37:57 +0500

    новый ген

--- экз/gen.py
+++ экз/gen.py
@@ -1,8 +1,17 @@
 import random
 
 numbers = list(range(1, 33))
-weights = [10 if x in [6, 11, 13, 14, 31, 32] else 8 if 16 <= x <= 25 else 1 for x in numbers]
+weights = [6 if x in [6, 11, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 31, 32] else 1 for x in numbers]
 
 result = random.choices(numbers, weights=weights, k=5)
 result.sort()
-print(result)
+
+print("Сгенерированные числа:", result)
+
+priority_numbers = [6, 11, 13, 14, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 31, 32]
+priority_found = [num for num in result if num in priority_numbers]
+
+if priority_found:
+    print("Приоритетные числа:", priority_found)
+else:
+    print("Приоритетные числа не выпали")
